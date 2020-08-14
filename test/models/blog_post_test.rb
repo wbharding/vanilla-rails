03e474b63ee6b2bd61c00fe33b90089466242abd
Bill Harding <wbharding@gmail.com>
2020-08-14 08:14:59 -0700

    Add generic BlogPost model

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    byebug
+    blog_post = BlogPost.create(title: "Blog post", user_id: 1)
+
+    assert true
+    blog_post.update(title: "Something else")
+  end
 end
