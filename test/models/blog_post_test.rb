aa814424c9a91272167ad422afbad35e4f68ede8
Bill Harding <wbharding@gmail.com>
2020-08-14 08:16:33 -0700

    Tweak test a little bit

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class BlogPostTest < ActiveSupport::TestCase
   test "the truth" do
     byebug
+    assert true # Proof that byebug can step over statements that aren't a model creation
     blog_post = BlogPost.create(title: "Blog post", user_id: 1)
 
     assert true
